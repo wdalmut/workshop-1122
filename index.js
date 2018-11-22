2e5549754870953a58fa903f9ed9cf3960962672
Walter Dal Mut <walter.dalmut@gmail.com>
2018-11-22 14:29:12 +0100

    TICKET-1 - CIao mondo

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 8000
 
-app.get('/', (req, res) => res.send('Ciao Mondo <h1>OK</h1>'))
+app.get('/', (req, res) => res.send('Ciao Mondo <h1>Ticket 1</h1>'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
