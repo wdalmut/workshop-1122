49df0c506ceed572729eb1a470fbdf0bc45618f4
Walter Dal Mut <walter.dalmut@gmail.com>
2018-11-22 14:24:39 +0100

    Added new title

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 8000
 
-app.get('/', (req, res) => res.send('Nuova release!'))
+app.get('/', (req, res) => res.send('Ciao Mondo <h1>OK</h1>'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
